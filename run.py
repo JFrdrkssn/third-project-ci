2d637e2eda9f5c2057cda5ccba6691b4f9baaacb
JFrdrkssn <johanf9401@gmail.com>
2022-02-24 12:13:27 +0000

    Update the board_print and guess methods to include more information to player

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from random import randint
 
+
 def random_coord(size):
     """
     This function helps with providing a random integer
@@ -10,11 +11,11 @@ def random_coord(size):
     col = randint(0, size - 1)
     return col, row
 
+
 class Board:
     """
     The Board class contains almost everything for the game to run. 
     """
-
     def __init__(self, size, num_ships, name, player=False):
         self.size = size
         self.num_ships = num_ships
@@ -26,23 +27,34 @@ class Board:
         # Stores the guesses (co-ordinates)
         self.guesses = []
         self.rounds = 16
-    
 
-    def boards(self):
+    def board_print(self):
         """
-        Prints the game boards for both player and computer
+        Prints the game boards for both the player and computer.
+        It adds co-ordinate numbers on the and left of the board.
         """
-        print(f"Captain {self.name}, ships are in formation!")
+        print(f"\n{self.name.capitalize()}'s board\n")
+        print("  0 1 2 3 4 5 6 7")
+        row_number = 0
         for row in self.board:
             print(" ".join(row))
+            row_number += 1
     
     def guess(self, row, col):
+        """
+        Marks misses (M) and hits (*) on the board.
+        Prints info to the player.
+        Stores already guessed co-ordinates
+        in the "guesses" list.
+        """
         self.guesses.append((row, col))
-        self.board[row][col] = "X"
+        self.board[row][col] = "M"
+        print(f"\nYou entered {row} and {col}.")
 
         if (row, col) in self.ships:
             self.board[row][col] = "*"
-            return "Hit confirmed, Captain {self.name}!"
+            print(f"Hit confirmed, Captain {self.name.capitalize()}!"
         else:
-            return "Target missed, Captain!"
+            print("Target missed, sir!")
+            
 
